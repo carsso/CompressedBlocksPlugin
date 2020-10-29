9090b28805424cde07c8c1931ad7b88285992781
SonarBeserk <sonarbeserk@gmail.com>
2020-10-29 00:55:39 -0400

    Remove old imports, Add support for 1.16.5

--- compileApiJars.bat
+++ compileApiJars.bat
@@ -9,5 +9,14 @@ echo "Running v1.13.2 build"
 java -jar BuildTools.jar --rev 1.13.2
 
 echo "Installing v1.13.2 locally"
-cmd /c mvn -B install:install-file -Dfile=spigot-1.13.jar -DgroupId=org.spigotmc -DartifactId=spigot -Dversion=1.13.2-R0.1-SNAPSHOT -Dpackaging=jar
+cmd /c mvn -B install:install-file -Dfile=spigot-1.13.2.jar -DgroupId=org.spigotmc -DartifactId=spigot -Dversion=1.13.2-R0.1-SNAPSHOT -Dpackaging=jar
+
+pause
+
+echo "Running v1.16.5 build"
+java -jar BuildTools.jar --rev 1.16.5
+
+echo "Installing v1.16.5 locally"
+cmd /c mvn -B install:install-file -Dfile=spigot-1.16.5.jar -DgroupId=org.spigotmc -DartifactId=spigot -Dversion=1.16.5-R0.1-SNAPSHOT -Dpackaging=jar
+
 pause
