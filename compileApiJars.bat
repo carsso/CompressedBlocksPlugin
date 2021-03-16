ee6abc40325b95c8bad78065b5f167577198486d
SonarBeserk <sonarbeserk@gmail.com>
2021-03-16 17:45:11 -0400

    Correct support for 1.16.5 by breaking out into v1_16_R3 module

--- compileApiJars.bat
+++ compileApiJars.bat
@@ -13,6 +13,14 @@ cmd /c mvn -B install:install-file -Dfile=spigot-1.13.2.jar -DgroupId=org.spigot
 
 pause
 
+echo "Running v1.16.3 build"
+java -jar BuildTools.jar --rev 1.16.3
+
+echo "Installing v1.16.3 locally"
+cmd /c mvn -B install:install-file -Dfile=spigot-1.16.3.jar -DgroupId=org.spigotmc -DartifactId=spigot -Dversion=1.16.3-R0.1-SNAPSHOT -Dpackaging=jar
+
+pause
+
 echo "Running v1.16.5 build"
 java -jar BuildTools.jar --rev 1.16.5
 
